af9320425f96361e60b9877d1fa893dea1367a40
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-16 08:26:14 +0900

    change tomcat script

--- userdata_file/make_tomcat.sh
+++ userdata_file/make_tomcat.sh
@@ -4,7 +4,6 @@ yum update -y ca-certificates
 yum install -y epel-release
 yum install -y salt-minion
 
-domain=$(hostname -f | awk -F'.' '{print $2"."$3}')
-echo "master: master.$domain" >> /etc/salt/minion
+echo "master: 1.1.1.1" >> /etc/salt/minion
 chkconfig salt-minion on
 service salt-minion start
