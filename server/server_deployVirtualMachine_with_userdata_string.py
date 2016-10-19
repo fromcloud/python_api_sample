4023ad5bdc2d25005214d5452b8d687b5bee3b93
Seok Woong Han <outspace99@gmail.com>
2016-10-19 14:31:47 +0900

    stop iptables for test

--- server/server_deployVirtualMachine_with_userdata_string.py
+++ server/server_deployVirtualMachine_with_userdata_string.py
@@ -41,6 +41,7 @@ userdata='''#!/bin/bash
 yum update -y ca-certificates
 yum install -y epel-release
 yum install -y httpd && chkconfig httpd on && service httpd start
+service iptables stop
 
 '''
 
